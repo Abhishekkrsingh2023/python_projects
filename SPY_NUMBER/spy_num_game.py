8f07747ac63bab1de184de7893b6e3a33fda20c7
Abhishekkrsingh2023 <abhishekkrsingh2023kolkata@gmail.com>
2024-05-27 14:57:12 +0530

    made changes

---
+++ SPY_NUMBER/spy_num_game.py
@@ -0,0 +1,42 @@
+'''
+************** PROGRAM TO FIND A SPY NUMBER **************
+'''
+''' 
+NOTE : A SPY number is a number where the sum
+          of digits is equal to the product of the 
+          digits
+'''
+# loop for continuing the game
+def sum(a): # gets the sum of digits
+    add=0
+    while(a!=0):
+        add+=(a%10);
+        a//=10
+    return add;
+
+def product(a): # gets the product of digits
+    pro=1;
+    while(a!=0):
+        pro*=(a%10);
+        a//=10;
+    return pro;
+
+while(True):
+    print('''
+        press 1 to play :
+        press 2 to exit :
+
+      ''')
+    op=int(input("Enter your choice : "))
+    if(op==1):
+        print("The GAME has Began!!!!")
+        # user choice 
+        num=int(input("Enter the number :"))
+
+        # print(sum(num))
+        if(sum(num)==product(num)):
+            print("Correct guess , YOU WIN")
+        else:
+            print("Wrong guess , Better luck next time")
+    else:
+        break;
